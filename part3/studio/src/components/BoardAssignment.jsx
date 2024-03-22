942eaf01c29f3bb9d9e302b5fafe5fb909df5eb1
Craig Brummer <craigbrummer28@gmail.com>
2024-03-21 20:07:09 -0500

    studio done

--- part3/studio/src/components/BoardAssignment.jsx
+++ part3/studio/src/components/BoardAssignment.jsx
@@ -1,18 +1,35 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-export default function BoardAssignment () {
-   const boards = [];
+export default function BoardAssignment() {
+  const [boardName, setBoardName] = useState("No boards yet!");
 
-   const handleChange = (event) => {
-   }
+  const boards = [
+    { label: "Breakfast", value: "breakfast" },
+    { label: "Quick Breads", value: "quickBreads" },
+    { label: "Weekend Baking", value: "weekendBaking" },
+  ];
 
-   return (
-      <div style={{paddingTop: "50px"}}>
+  const boardOptions = boards.map((board, index) => {
+    return (
+      <option key={index} value={board.value}>
+        {board.label}
+      </option>
+    );
+  });
+
+  const handleChange = (event) => {
+setBoardName(event.target.value)
+
+  };
+
+  return (
+    <div style={{ paddingTop: "50px" }}>
       <label>Save to Board: </label>
       <select value={boardName} onChange={handleChange}>
+        {boardOptions}
       </select>
 
       <p>Saved to {boardName}!</p>
-      </div>
-   );
+    </div>
+  );
 }
--- part3/studio/src/components/Recipe.jsx
+++ part3/studio/src/components/Recipe.jsx
@@ -1,65 +1,67 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const RecipeAuthor = () => {
-   let authorLink = "";
-   let authorPhoto = "";
-   let authorName = "";
+  let authorLink = "https://sallysbakingaddiction.com/about/";
+  let authorPhoto =
+    "https://sallysbakingaddiction.com/wp-content/uploads/2023/09/sally-mckenney-1.jpg";
+  let authorName = "Sally McKenney";
 
-   return (
+  return (
+    <div>
+      <img
+        src={authorPhoto}
+        alt=""
+        style={{objectFit: "contain", borderRadius: "50%"}}
+        width = "200px"
+      />
       <div>
-         <img src={authorPhoto} alt = "" style={{objectFit: "contain", borderRadius: "50%"}} />
-         <div>
-            <h3>{authorName}</h3>
-            <a href={authorLink}></a> 
-         </div>
+        <h3>{authorName}</h3>
+        <a href={authorLink}></a>
       </div>
-   );
-}
+    </div>
+  );
+};
 
 const RecipeIngredients = () => {
-   const ingredients = [];
-   return(
-      <div>
-         <h3>Recipe Ingredients</h3>
-         <ul>
-            <li>{ingredients[0]}</li>
-            <li>{ingredients[1]}</li>
-            <li>{ingredients[2]}</li>
-            <li>{ingredients[3]}</li>
-            <li>{ingredients[4]}</li>
-         </ul>
-      </div>
-   );
-}
+  const ingredients = ["brown sugar", "flour", "butter", "eggs", "cinnamon", "vanilla"];
+
+  const ingredientListItems = ingredients.map((ingredient, index) => {return <li key={index}>{ingredient}</li>})
+  return (
+    <div>
+      <h3>Recipe Ingredients</h3>
+      <ul>
+      {ingredientListItems}
+      </ul>
+    </div>
+  );
+};
 
 const RecipeDescription = () => {
-   return (
-      <div> 
-         <div>
-            <h1></h1>
-            <p></p>
-         </div>
-         <div className="recipePhotoBlock">
-            <RecipeIngredients />
-            <RecipeAuthor />
-         </div>
+  return (
+    <div>
+      <div>
+        <h1>Sour Cream Coffee Cake</h1>
+        <p>Simple and classic, this buttery cinnamon crumb coffee cake is anything but plain! Sour cream ensures a moist, light breakfast cake, and there is double the brown sugar cinnamon crumb streusel. Enjoy a thick ribbon of streusel inside and plenty of crumbs on top, both made from the same mixture.</p>
       </div>
-   );
-}
+      <div className="recipePhotoBlock">
+        <RecipeIngredients />
+        <RecipeAuthor />
+      </div>
+    </div>
+  );
+};
 
 const RecipePhoto = () => {
-   return (
-      <img src="" alt="" className="imageUpdates"/>
-   );
-}
+  return <img src="https://sallysbakingaddiction.com/wp-content/uploads/2015/05/crumb-coffee-cake.jpg" alt="Sour Cream Coffee Cake" className="imageUpdates" height = "400px"/>;
+};
 
-export default function RecipeDisplay () {
-   return(
-      <div className="recipePhotoBlock">
-         <RecipePhoto />
-         <div>
-            <RecipeDescription />
-         </div>
+export default function RecipeDisplay() {
+  return (
+    <div className="recipePhotoBlock">
+      <RecipePhoto />
+      <div>
+        <RecipeDescription />
       </div>
-   );
+    </div>
+  );
 }
